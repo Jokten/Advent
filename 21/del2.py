47aaf81080a003eff1e61a4a41111c5d87a9f72d
Jokten123 <Jokten123@gmail.com>
2021-12-21 13:35:32 +0100

    21 klarad

--- 21/del2.py
+++ 21/del2.py
@@ -1,11 +1,9 @@
-
-
 def roll_dice(turn, points, position, universe, dice_roll):
     points = points.copy()
     position = position.copy()
 
     universe *= dice_roll[0]
-    position[turn] = (position[turn] + dice_roll[0])%10
+    position[turn] = (position[turn] + dice_roll[1])%10
     points[turn] += position[turn] + 1
     if points[turn] >= 21:
         total_universe[turn] += universe
@@ -21,7 +19,7 @@ def main():
     global total_universe
     total_universe = [0, 0]
     universe = 1
-    positions = [4, 10]
+    positions = [3, 9]
     points = [0, 0]
     for i in possible:
         roll_dice(0, points, positions, universe, i)

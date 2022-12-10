7531ac906ccd6f1e9bd6bf93a8d42737c0c4a285
Jokten123 <Jokten123@gmail.com>
2022-12-10 12:31:02 +0100

    kod

--- 2022/9/9.py
+++ 2022/9/9.py
@@ -1,5 +1,4 @@
 import numpy as np
-import itertools
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)).split('2022')[0])
@@ -13,14 +12,12 @@ def move_to(pos_head, pos_tail):
 
 def main():
     data = aoctools.data_loader(2022, 9, two_parts=False)
-    visited = set()
+    visited, visited2 = set(), set()
     visited.add((0,0))
-    visited2 = set()
     visited2.add((0,0))
     pos_tails = [[0,0]]
     num_tails = 9
-    for i in range(num_tails):
-        pos_tails.append([0,0])
+    for i in range(num_tails): pos_tails.append([0,0])
     for i in data:
         ins = i.split(' ')
         for j in range(int(ins[1])):

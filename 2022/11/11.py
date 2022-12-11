ab705bd86157ea3cfb44c1108b4dc81ce1933d9f
Jonathan <Jokten123@gmail.com>
2022-12-11 09:35:41 +0100

    kod

---
+++ 2022/11/11.py
@@ -0,0 +1,48 @@
+import re
+import math
+class Monkey:
+    def __init__(self, data, monkeys):
+        self.name = int(re.findall(r'Monkey (\d):$', data[0])[0])
+        self.items = [int(i) for i in re.findall(r'(\d{2})', data[1])]
+        self.op = re.findall(r'new = (old .*)$', data[2])[0]
+        self.div = int(re.findall(r'(\d+)$', data[3])[0])
+        self.true = int(re.findall(r'(\d+)$', data[4])[0])
+        self.false = int(re.findall(r'(\d+)$', data[5])[0])
+        self.monkeys = monkeys
+        self.inspects = 0
+
+    def throw(self):
+        for old in self.items:
+            new = eval(self.op)%self.mods#//3
+            if new % self.div == 0:
+                self.monkeys[self.true].items.append(new)
+            else:
+                self.monkeys[self.false].items.append(new)
+            self.inspects += 1
+        self.items = []
+
+
+def main():
+    with open('2022\\11\input.txt', 'r') as file:
+        data = file.read()
+    data = data.split('\n\n')
+    monkeys = []
+    for i in data:
+        monkeys.append(Monkey(i.splitlines(), monkeys))
+    divs = []
+    for monkey in monkeys: divs.append(monkey.div)
+    mods = math.prod(divs)
+    for monkey in monkeys: monkey.mods = mods
+
+    for i in range(10000):
+        for monkey in monkeys:
+            monkey.throw()
+    inspects = []
+    for monkey in monkeys:
+        inspects.append(monkey.inspects)
+    inspects.sort()
+    print(inspects)
+    print(inspects[-1]*inspects[-2])
+
+if __name__ == '__main__':
+    main()

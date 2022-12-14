f9043cdcb81edd07c6637c45c4f8fd63741c5cd2
Jokten123 <Jokten123@gmail.com>
2022-12-14 14:26:35 +0100

    fa

--- 2022/14/14.py
+++ 2022/14/14.py
@@ -1,5 +1,5 @@
-import numpy as np
-import re
+from functools import cache
+from time import perf_counter
 
 def parser():
     with open(r'2022\14\input.txt', 'r') as openfile:
@@ -7,11 +7,7 @@ def parser():
     rocks = set()
     for row in data:
         r = []
-        row = row.split(' -> ')
-        for cords in row:
-            cords = cords.split(',')
-            cords = [int(i) for i in cords]
-            r.append(tuple(cords))
+        for cords in row.split(' -> '): r.append(tuple([int(i) for i in cords.split(',')]))
         for k in range(len(r)-1):
             upx = max([r[k][0], r[k+1][0]])
             downx = min([r[k][0], r[k+1][0]])
@@ -22,14 +18,33 @@ def parser():
                     rocks.add((i, j))
     return rocks
 
+@cache
 def neighbours(cords):
-    if 
+    return [(cords[0], cords[1]+1), (cords[0]-1, cords[1]+1), (cords[0]+1, cords[1]+1)]
             
-    # 169
-
 def main():
-    data = parser()
-    
+    occupied = parser()
+    void = 1
+    tot = 0
+    part1= 0
+    while void:
+        cur = (500, 0)
+        while True:
+            ne = [i for i in neighbours(cur) if (i not in occupied) and i[1] < 171]
+            if ne: cur = ne[0]
+            else:
+                occupied.add(cur)
+                tot += 1
+                break
+            if cur[1] >= 169 and part1 == 0:
+                print('Part 1: ',tot)
+                part1 = 1
+        if (500, 0) in occupied:
+            void = 0
+    print('Part 2: ',tot)
+
 
 if __name__ == '__main__':
+    start = perf_counter()
     main()
+    print(f'Time: {perf_counter()-start}')

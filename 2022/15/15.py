865c731f509c6d8cdc8763828861b376e3bfb647
Jokten123 <Jokten123@gmail.com>
2022-12-15 13:32:53 +0100

    kod

--- 2022/15/15.py
+++ 2022/15/15.py
@@ -8,57 +8,47 @@ def find_possible(y,dists):
         if abs(y-i[1]) <= j:
             diff = abs(y-i[1])-j
             ren = (i[0]+diff, i[0]-diff)
-            print(ren)
-            if ran == []:
-                print('onve')
-                ran.append(ren)
-            new_ran = ran.copy()
-            to_remove = []
-            for i in ran:
-                print('here',i)
-                if i[0] <= ren[0] <= i[1] or i[0] <= ren[1] <= i[1]:
-                    to_remove.append(i)
-                    new_ran.append((min(i[0], ren[0]), max(i[1], ren[1])))
-                    break
-                else:
-                    ran.append(ren)
-            print('ran',new_ran)
-            print('rem',to_remove)
-            for i in to_remove:
-                new_ran.remove(i)
-            ran = new_ran
-            print(ran)
-    s = 0
-    print(ran)
+            ran.append(ren)
+    ran.sort(key=lambda x: x[0])
+    new= []
+    cur=None
     for i in ran:
-        s += i[1]-i[0]+1
-    print(s)
+        if cur is None: cur = i
+        elif cur[1]+1 >= i[0]: cur = (cur[0], max(i[1], cur[1]))
+        else:
+            new.append(cur)
+            cur = i
+    new.append(cur)
+    return new
 
 def main():
+    parse_start = time.perf_counter()
     with open(r'2022\15\input.txt', 'r') as openfile:
         data = openfile.read().splitlines()
-    data = [re.findall(r'-?\d+', i) for i in data]
-    beac = {}
+    data = [[int(j) for j in re.findall(r'-?\d+', i)] for i in data]
     dists = {}
-    thousand = set()
-    ref = 2000
-    for i in data:
-        beac[(int(i[0]),int(i[1]))] = (int(i[2]), int(i[3]))
-        dists[(int(i[0]),int(i[1]))] = abs(int(i[0])-int(i[2])) + abs(int(i[1])-int(i[3]))
-    bc = set(beac.values())
+    beacs = set()
+    ref = 4000000
+    y1 = 2000000
+    dists = {(x, y): abs(x-a) + abs(y-b) for x, y, a, b in data}
+    beacs = {(a, b) for x, y, a, b in data}
+    parse_time = time.perf_counter() - parse_start
+    # part 1
+    ranges = find_possible(y1, dists)
+    print(sum([i[1]-i[0]+1 for i in ranges])-sum([1 for i in dists.keys() if i[1] == y1]) - sum([1 for i in beacs if i[1] == y1]))
 
-    # for i in dists:
-    #     diff =   abs(ref-i[1])
-    #     if diff < dists[i]:
-    #         for j in range(dists[i]-diff+1):
-    #             thousand.add((i[0]+j, ref))
-    #             thousand.add((i[0]-j, ref))
-    # print(thousand.intersection(bc))
-    # print(len(thousand))
-    # print(len(thousand)-len(thousand.intersection(bc)))
-    find_possible(10, dists)
-        
+    # part 2
+
+    for i in range(ref):
+        ranges = find_possible(i, dists)
+        red = [k for k in ranges if 0<k[1] and k[0]<ref]
+        if len(red) > 1:
+            print((red[0][1]+1),i)
+            print((red[0][1]+1)*4000000+i)
+            break
     
             
 if __name__ == '__main__':
+    sta = time.perf_counter()
     main()
+    print(f'Time: {time.perf_counter()-sta}')
